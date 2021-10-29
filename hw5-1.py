b3686017c0f5edd7914f0b94fe53c6de93ac3a1b
p22strautz <p22strautz@fairfieldprep.org>
2021-10-29 11:04:14 -0400

    hw5-1 submit

--- hw5-1.py
+++ hw5-1.py
@@ -30,3 +30,9 @@ print(random.sample([1, 2, 3, 4, 5], 5))
 
 # Question 9
 cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
+random.shuffle(cards)
+print(cards)
+
+# Question 10
+random.seed(1942)
+print(random.randint(1, 1000))
